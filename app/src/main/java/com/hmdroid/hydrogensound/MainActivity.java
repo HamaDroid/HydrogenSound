db45b45edf874525e98c8d69fa98fe80e54af39a
HamaDroid <daichithirteen@gmail.com>
2018-03-31 21:50:17 +0900

    Initial commit

---
+++ app/src/main/java/com/hmdroid/hydrogensound/MainActivity.java
@@ -0,0 +1,18 @@
+package com.hmdroid.hydrogensound;
+
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.support.v7.app.AppCompatActivity;
+import android.R;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
